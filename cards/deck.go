648cf161b3b7666c619b20f5348520a1a0a6512c
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-07-16 18:51:23 +0100

    creating deal, toString, saveToFile, and new deck from file functions

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"os"
+)
 
 // create a new type of 'deck' which is a slice of strings
 type deck []string
@@ -23,4 +27,30 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck){
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ", ")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		// option #1 - log the error and return a call to newDeck()
+		// option #2 - log the error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split((string(bs)), ", ")
+	
+	return deck(s)
 }
--- cards/main.go
+++ cards/main.go
@@ -3,8 +3,7 @@ package main
 // import "fmt"
 
 func main() {
-	cards := newDeck()
-	
+	cards := newDeckFromFile("my_cards")
+
 	cards.print()
 }
-
