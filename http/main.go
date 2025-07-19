a0827c4e7b300101887ef3e3e924eb023d4005f0
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-07-19 20:00:11 +0100

    learning http and interface to understand how it works

--- http/main.go
+++ http/main.go
@@ -1,2 +1,19 @@
-package main 
+package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err :=  http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error fetching URL:", err)
+		os.Exit(1)
+	}
+
+	bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+}
